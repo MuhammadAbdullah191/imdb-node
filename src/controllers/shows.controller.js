7191465893767b94f0a6fe79c3f60b434a22f63c
MUHAMMADABDULLAH191 <abdullah.badar@devsinc.com>
2022-12-05 17:27:25 +0500

    uplading upload preset

--- src/controllers/shows.controller.js
+++ src/controllers/shows.controller.js
@@ -21,7 +21,7 @@ exports.createShow = async(req,res) =>{
 			// req.body.images=[]
 			if (fileStrs){
 				for (const file of fileStrs) {
-					uploadedResponse = await cloudinary.uploader.upload(file)
+					uploadedResponse = await cloudinary.uploader.upload(file,{upload_preset:'Cloudinary'})
 					// req.body.images.push(uploadedResponse.url)
 				}
 			}
